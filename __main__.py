4542e4505e2b720792c4b90631d251acc2f19ee4
TC_Linux <88120195+TcPirate1@users.noreply.github.com>
2025-04-10 22:58:44 +1200

    Initialize functions that the script will have

--- __main__.py
+++ __main__.py
@@ -1,7 +1,17 @@
 import platform
+import csv
 
-def discoverOS():
+def discover_os():
+    """Gets the OS name of the machine script is ran from"""
     platform.system()
 
+def read_csv():
+    """Reads csv file"""
+    pass
+
+def additional_args():
+    """Ask user whether they want to add additional arguments (for now only ask save location)"""
+    pass
+
 if __name__ == "__main__":
-    discoverOS()
+    discover_os()
