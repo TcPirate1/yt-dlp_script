c6fd8754f390000d972a0101c5ab2c0ddb0abb37
TC_Linux <88120195+TcPirate1@users.noreply.github.com>
2025-04-11 14:31:55 +1200

    pylint config file added. Added functionality for asking location of file.

--- __main__.py
+++ __main__.py
@@ -1,17 +1,39 @@
 import platform
 import csv
+import os
+import subprocess
+
 
 def discover_os():
     """Gets the OS name of the machine script is ran from"""
-    platform.system()
+    print(platform.system())
+
+
+def file_location():
+    """Ask user for file location"""
+    user_input = input("Where is the CSV file? ( Start from \"home/{username}\" or \"C:\\Users\\{username}\" )\n")
+    home_directory = os.path.expanduser('~')
+
+    try:
+        if (discover_os() == "Linux"):
+            if (os.path.exists(f'{home_directory}/{user_input}')):
+                subprocess.run([f'cd {home_directory}/{user_input}'])
+        else:
+            if (os.path.exists(f'{home_directory}\\{user_input}')):
+                subprocess.run([f'cd {home_directory}\\{user_input}'])
+    except:
+        print("Pathing error")
+
+
 
 def read_csv():
     """Reads csv file"""
-    pass
+    file_location()
+
 
 def additional_args():
     """Ask user whether they want to add additional arguments (for now only ask save location)"""
-    pass
+
 
 if __name__ == "__main__":
-    discover_os()
+    read_csv()
